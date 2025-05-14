f7c52225ca793d873cd290fac31bbd6814ab191d
Rafael Correa <rafaelscorreadev@gmail.com>
2025-05-14 12:36:06 -0300

    Remove rota update_server temporariamente para corrigir erro de deploy

--- bookstoreapp/urls.py
+++ bookstoreapp/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 
 import debug_toolbar
-from bookstoreapp import views
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
@@ -27,6 +26,4 @@ urlpatterns = [
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
-    path("update_server/", views.update, name="update"),
-    path("hello/", views.hello_world, name="hello_world"),
 ]
