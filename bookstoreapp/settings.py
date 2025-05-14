9241156b7eb3dcad20c026eccdac310b9d7803c4
Rafael Correa <rafaelscorreadev@gmail.com>
2025-05-14 12:52:47 -0300

    Ajuste nos middlewares

--- bookstoreapp/settings.py
+++ bookstoreapp/settings.py
@@ -49,13 +49,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "bookstoreapp.urls"
